93f348e1e9bf2cde1cdad370043cadcbbc4e4524
Daniel Neset <danielneset@gmail.com>
2025-05-01 04:07:40 +0200

    Initial spring boot commit

---
+++ src/main/java/no/ntnu/gr10/bachelor_producer_rest_api/BachelorProducerRestApiApplication.java
@@ -0,0 +1,13 @@
+package no.ntnu.gr10.bachelor_producer_rest_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BachelorProducerRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BachelorProducerRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/no/ntnu/gr10/bachelor_producer_rest_api/BachelorProducerRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package no.ntnu.gr10.bachelor_producer_rest_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BachelorProducerRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
